6ce95681c74133a746687617c67f2ffcba4e5f36
Mohammed Snoussi <mohammedsnoussi@MacBook-Pro-de-Mohammed.local>
2023-11-11 16:19:00 +0100

    Initial commit

---
+++ fonctions.py
@@ -0,0 +1,160 @@
+#################################################################
+
+# Importation des modules nécessaires pour certaines fonctions
+from math import log
+from os import listdir
+
+
+#################################################################
+
+# Toutes les fonctions de bases
+def repertoire_fichiers(s):
+    """
+    Fonction qui récupére tous les fichiers présents d'un certain répertoire
+    :param s: chaine de caractère représentant le répertoire où les fichiers du corpus sont présent
+    :return: renvoie une liste contenant les noms de chaque fichiers présent dans le répertoire s
+    """
+    return [f for f in listdir(f"{s}")]
+    #liste en compréhension qui s'occupe de prendre chaque element du répertoire s
+
+
+def exctraction_nom(f):
+    """
+
+    :param f: Fichiers sous forme de liste
+    :return: renvoie les noms des présidents sous forme de liste
+    """
+    nom_president = []
+    for fichier in f:
+        president = ""
+        for lettre in fichier[11:]:
+            if (48 <= ord(lettre) <= 57) or (lettre == "."):
+                break
+            else:
+                president += lettre
+        if president not in nom_president:
+            nom_president.append(president)
+    return nom_president
+
+
+def conversion_mini(f):
+    """
+    Fonction qui convertit le texte des fichiers en miniscules dans de nouveaux fichier dans le dossier cleaned
+    :param f: noms des fichiers sous forme de liste
+    :return: renvoie rien et créer les noueaux fichiers dans le dossier cleaned
+    """
+    for fichiers in f:
+        with open(f"speeches/{fichiers}", "r") as fichier:
+            contenu = fichier.readlines()
+        fichier_new = open(f"cleaned/{fichiers}", "w")  # Creation du nouveau fichier dans le dossier 'cleaned'
+        for ligne in contenu:
+            fichier_new.write(ligne.lower())
+        fichier_new.close()
+    return None
+
+
+def clean_fichier(f):
+    """
+    Fonction qui "clean" les fichiers afin de supprimer tous les caractéres spéciaux laissant qu'un fichier
+    contenant des mots séparé par des espaces
+    :param f: Nom des fichiers sous forme de list
+    :return: Renvoie rien, change le contenue des fichiers
+    """
+    for fichiers in f:
+        with open(f"cleaned/{fichiers}", "r") as fichier:
+            contenu = fichier.readlines()
+        fichier = open(f"cleaned/{fichiers}",
+                       "w")  # Réécriture du fichier apres avoir récupérer le contenue (d'où le "w")
+        for ligne in contenu:
+            new_ligne = ""
+            for caractere in ligne:
+                if caractere in "'-":
+                    new_ligne += " "
+                elif caractere not in ',;!.?:"':
+                    new_ligne += caractere
+            fichier.write(new_ligne)
+        fichier.close()
+
+    return None
+
+
+#################################################################
+
+# La méthode TF-IDF
+def occ_mots(c):
+    """
+    Fonction qui compte l'occurence d'un mots dans une chaine de caractere et renvoie un dictionnaire
+    associant à chaque mot le nombre de fois qu’il apparait dans la chaine de caractères.
+    :param c: chaine de caractères
+    :return: dictionnaire {chaque mots : occurence du mots}
+    """
+    dic = {}
+    mots = c.split()  # création d'une liste correspondant au mots de la chaine de caractères
+    for i in mots:
+        occ = 0
+        if i not in dic.keys(): # Afin d'éviter de faire des boucles inutiles si le mots a déjà été traité
+            for j in mots:
+                if j == i:
+                    occ += 1
+            dic[i] = occ
+    return dic
+
+
+def idf_mots(s):
+    """
+    Fonction qui calclule le score IDF de chaque mots des fichiers d'un corpus
+    :param s: chaine de caractère représentant le répertoire où les fichiers du corpus sont présent
+    :return: un dictionnaire {mot : score IDF}
+    """
+    mots_fichier = mots_fichiers(s)
+    for i in mots_fichier:
+        mots = mots_fichier[i]
+        for
+
+
+
+
+def mots_par_fichiers(s):
+    """
+    fonction qui récupére chaque mots d'un fichier les mettant dans une liste et les renvoyant
+    sous forme d'un dic
+    :param s: chaine de caractère représentant le répertoire où les fichiers du corpus sont présent
+    :return: un dictionnaire {  fichier : [tous les mots du fichiers sous forme de liste] }
+    """
+
+    fichiers = repertoire_fichiers(s)
+    # Récupération des noms de chaque fichiers dans le répertoire s
+
+    dic = {}
+
+    for fichier in fichiers: # Pour chaque fichier dans le répertoire
+        with open(fichier, "r") as f1:
+            mots = []
+            contenues = f1.readlines()
+            for ligne in contenues:
+                mots = mots + ligne.split()
+            dic[fichier] = mots
+
+    return dic
+
+
+
+def mots_fichiers(s):
+    """
+    fonction qui récupére chaque mots de tous les fichiers présent dans le répertoire,
+    il récupére une fois chaque mots !!
+    :param s: chaine de caractère représentant le répertoire où les fichiers du corpus sont présent
+    :return: une liste contenant tous les mots UNIQUES (sans doublons) présent des tous les fichiers
+    """
+    fichiers = [f for f in listdir(f"{s}")]
+    # Récupération des noms de chaque fichiers dans le répertoire s
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+from os import listdir, mkdir
+#from fonctions import *
+
+
+
+#Création du dossier "cleaned"
+if "cleaned" not in listdir("/Users/mohammedsnoussi/PycharmProjects/Projet_ChatBot"):
+    mkdir('cleaned')
+
+
+#print(exctraction_nom(fichiers))
+
+#conversion_mini(fichiers)
+
+#clean_fichier(fichiers)
+
+
+
+
+
+
+
